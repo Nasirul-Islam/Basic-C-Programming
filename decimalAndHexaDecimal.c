e9bcd7c86b768a9d56d7f1883e445fdee886484b
Nasirul-Islam <nasirulislam866@gmail.com>
2022-02-25 00:33:27 +0600

    conversion between decimal and hexa-decimal

---
+++ decimalAndHexaDecimal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    // decimal to hexa-decimal
+    /*
+    printf("type any decimal number: ");
+    scanf("%d", &number);
+    printf("Hexa-decimal is: %x", number);
+    */
+    // hexa-decimal to decimal
+    printf("type Hexa-decimal: ");
+    scanf("%x", &number);
+    printf("Decimal is: %d", number);
+}
