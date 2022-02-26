d298516bc1391bb6b0f5d6cb91ec4998abf1edbe
Nasirul-Islam <nasirulislam866@gmail.com>
2022-02-26 17:34:26 +0600

    sum two integer

---
+++ sumtwonum.c
@@ -0,0 +1,11 @@
+// write a program that take two integer and display sum
+#include<stdio.h>
+int main()
+{
+    int num1, num2, sum;
+    printf("type to integer: ");
+    scanf("%d %d", &num1, &num2);
+    sum = num1 + num2;
+    printf("there sum is: %d", sum);
+    return 0;
+}
