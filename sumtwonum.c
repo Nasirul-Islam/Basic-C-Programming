3c5248649a985113958121e4638fd9b20c69b710
Nasirul-Islam <nasirulislam866@gmail.com>
2022-02-26 17:49:29 +0600

    find Average

--- sumtwonum.c
+++ sumtwonum.c
@@ -3,9 +3,16 @@
 int main()
 {
     int num1, num2, sum;
+    float avg;
+
     printf("type to integer: ");
     scanf("%d %d", &num1, &num2);
+
     sum = num1 + num2;
     printf("there sum is: %d", sum);
+
+    avg = (float)sum/2;
+    printf("The Average is: %.2f\n", avg);
+
     return 0;
 }
