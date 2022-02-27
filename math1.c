62afc41802700325b2ace2a1de9b97cd3ba00984
Nasirul-Islam <nasirulislam866@gmail.com>
2022-02-27 11:52:14 +0600

    more library function for math opration

---
+++ math1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    double x = 1.3;
+
+    //double result = log(x);
+    //double result = log10(x);
+    //double result = exp(x);
+    //double result = sin(x);
+    //double result = cos(x);
+    //double result = round(x);
+    //double result = trunc(x);
+    //double result = ceil(x);
+    double result = floor(x);
+
+    printf("result = %lf\n", result);
+
+    return 0;
+}
