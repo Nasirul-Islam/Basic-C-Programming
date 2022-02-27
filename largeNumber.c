948f61455238a0e531306a18598099568d18f692
Nasirul-Islam <nasirulislam866@gmail.com>
2022-02-27 13:06:16 +0600

    large number

---
+++ largeNumber.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int num1, num2;
+    printf("Enter two integer number: ");
+    scanf("%d %d", &num1, &num2);
+
+    if(num1>num2)
+    {
+        printf("Large number = %d", num1);
+    }
+    else if(num1<num2)
+    {
+        printf("Large number = %d", num2);
+    }
+    else
+    {
+        printf("Both are Equal");
+    }
+}
