1df3e5691bbd7c3dc97098eca3028149582a49d4
Nasirul-Islam <nasirulislam866@gmail.com>
2022-02-27 15:34:32 +0600

    vowel or consonant

---
+++ vowelOrConsonant.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+    {
+        printf("Character is Vowel ");
+    }
+    else
+    {
+        printf("Character is Consonant");
+    }
+
+    return 0;
+}
