c5197984a80c7431011424dca10242d3946b5133
Nasirul-Islam <nasirulislam866@gmail.com>
2022-03-14 23:57:16 +0600

    many type of input output

---
+++ inputOutput.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    // Inputs ---
+    // char a = getchar();
+    // char a[100];
+    // gets(a);
+    char ch;
+    scanf("%c", &ch);
+    // char ch[100];
+    // scanf("%s", &ch);
+
+    // Outputs ---
+    printf("you entered: ");
+    // putchar(a);
+    // puts(a);
+    printf("%c", ch);
+    // printf("%s", ch);
+
+    return 0;
+}
